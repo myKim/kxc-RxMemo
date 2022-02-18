7fe388d0fbe1a20b8f55b563e11d7bdc95b6344c
myKim <myung120@gmail.com>
2022-02-18 15:52:08 +0900

    Step 13 - 화면 전환 구현

--- RxMemo/RxMemo/ViewModel/MemoListViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoListViewModel.swift
@@ -8,10 +8,40 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Action
 
 // 메모 목록 화면에서 사용하는 뷰 모델
 class MemoListViewModel: CommonViewModel {
     var memoList: Observable<[Memo]> {
         return storage.memoList()
+    }
+    
+    func makeCreateAction() -> CocoaAction {
+        return CocoaAction { _ in
+            return self.storage.createMemo(content: "")
+                .flatMap { memo -> Observable<Void> in
+                    let composeViewModel = MemoComposeViewModel(title: "새 메모", sceneCoordinator: self.sceneCoordinator, storage: self.storage, saveAction: self.performUpdate(memo: memo), cancelAction: self.performCancel(memo: memo))
+                    
+                    let composeScene = Scene.compose(composeViewModel)
+                    
+                    return self.sceneCoordinator.transition(to: composeScene, using: .modal, animated: true)
+                        .asObservable()
+                        .map { _ in }
+                }
+
+        }
+    }
+    
+    // Action 왼쪽이 입력 오른쪽이 출력, Observable이 방출하는 쪽이 오른쪽
+    func performUpdate(memo: Memo) -> Action<String, Void> {
+        return Action { input in
+            return self.storage.update(memo: memo, content: input).map { _ in }
+        }
+    }
+    
+    func performCancel(memo: Memo) -> CocoaAction {
+        return Action {
+            return self.storage.delete(memo: memo).map { _ in }
+        }
     }
 }
