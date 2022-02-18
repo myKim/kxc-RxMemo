c667a3f8d36cf0a6ac8498f100e7dae35e14e848
myKim <myung120@gmail.com>
2022-02-18 19:53:00 +0900

    Step 15 - MemoDetailViewModel 구현

--- RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,8 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
+
 
 // 메모 보기에서 사용
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        
+        return f
+    }()
+    
+    var contents: BehaviorSubject<[String]>
+    
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
