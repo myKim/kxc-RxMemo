9c134bd3019f1a939eccded32ddd5313605af7f6
myKim <myung120@gmail.com>
2022-02-19 17:00:24 +0900

    Step 18 - 메모 편집 구현

--- RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -43,4 +43,27 @@ class MemoDetailViewModel: CommonViewModel {
             .asObservable()
             .map { _ in }
     }
+    
+    func performUpdate(memo: Memo) -> Action<String, Void> {
+        return Action { input in
+            self.storage.update(memo: memo, content: input)
+                .map { [$0.content, self.formatter.string(from: $0.insertDate)] }
+                .bind(onNext: { self.contents.onNext($0) })
+                .disposed(by: self.rx.disposeBag)
+            
+            return Observable.empty()
+        }
+    }
+    
+    func makeEditAction() -> CocoaAction {
+        return CocoaAction { _ in
+            let composeViewModel = MemoComposeViewModel(title: "메모 편집", content: self.memo.content, sceneCoordinator: self.sceneCoordinator, storage: self.storage, saveAction: self.performUpdate(memo: self.memo))
+            
+            let composeScene = Scene.compose(composeViewModel)
+            
+            return self.sceneCoordinator.transition(to: composeScene, using: .modal, animated: true)
+                .asObservable()
+                .map { _ in }
+        }
+    }
 }
