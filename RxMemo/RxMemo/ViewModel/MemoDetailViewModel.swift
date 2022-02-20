efb37191788e01f15ae7bae193a3486974c770ac
myKim <myung120@gmail.com>
2022-02-20 12:53:07 +0900

    Step 23 - 편집 화면 오류 수정

--- RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -13,7 +13,7 @@ import Action
 
 // 메모 보기에서 사용
 class MemoDetailViewModel: CommonViewModel {
-    let memo: Memo
+    var memo: Memo
     
     private var formatter: DateFormatter = {
         let f = DateFormatter()
@@ -47,6 +47,7 @@ class MemoDetailViewModel: CommonViewModel {
     func performUpdate(memo: Memo) -> Action<String, Void> {
         return Action { input in
             self.storage.update(memo: memo, content: input)
+                .do(onNext: { self.memo = $0 })
                 .map { [$0.content, self.formatter.string(from: $0.insertDate)] }
                 .bind(onNext: { self.contents.onNext($0) })
                 .disposed(by: self.rx.disposeBag)
